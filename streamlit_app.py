80ad614127ed2f7ad36e5be03f40165f812066bd
chungiemo <142489896+chungiemo@users.noreply.github.com>
2023-08-17 10:49:18 +0100

    add user interactive widget

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
 
+# create title
 streamlit.title ('My Parents New Healthy Diner')
 
+# add menu header and 4 itmes
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
@@ -10,8 +12,15 @@ streamlit.text(' Avovado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
+# import csv from s3 bucket
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.dataframe(my_fruit_list)
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
