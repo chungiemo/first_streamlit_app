92974fec9bf50ae6d52734aedf44ddf53f3e78b0
chungiemo <142489896+chungiemo@users.noreply.github.com>
2023-08-17 10:43:26 +0100

    Update streamlit_app.py
    updated special mean header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 streamlit.text(' Avovado Toast')
 
-streamlit.header('Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
