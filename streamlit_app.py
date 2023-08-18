a5ad5435fd4df0064290e5e81a0bf4c7eea01f06
chungiemo <142489896+chungiemo@users.noreply.github.com>
2023-08-18 13:23:32 +0100

    reapplied

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,22 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected ]
 
 
+# Display the table on the page.
+streamlit.dataframe(fruits_to_show)
+
+#new section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi"")
+
+# take the json version of the response and normalize it
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# output it the screen as a table
+streamlit.dataframe(fruityvice_normalized)
+
+import snowflake.connector
+
+
