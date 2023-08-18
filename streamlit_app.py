7e35644d3161c2dd928905c04702664e36c0d0f9
chungiemo <142489896+chungiemo@users.noreply.github.com>
2023-08-18 16:13:03 +0100

    missing code

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,9 @@ streamlit.dataframe(fruits_to_show)
 
 #create the repeatable code block (called a function)
 def get_fruityvice_data(this_fruit_choice):
+     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+     fruityvice_normalized = pandas.json.normalize (fruityvice_response.json)))
+     return  fruityvice_normalized
 
 #new section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
@@ -39,7 +42,6 @@ try:
      if not fruit_choice:
         streamlit.error("Please selecct a fruit to get information.")
      else:
-         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
          back_from_function = get_fruityvice_data(fruit_choice)
          streamlit.dataframe(back_from_function)
 except URLError as e:
